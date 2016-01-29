3cfff20377dc2868e5bbbf1701d11d162267185d
mah88 <a.medhat.h@gmail.com>
2016-01-29 13:33:48 +0100

    succesful Json

--- src/main/java/org.project.test/Main.java
+++ src/main/java/org.project.test/Main.java
@@ -19,6 +19,7 @@ public class Main {
 
 
     public static void main(String[] args) throws FileNotFoundException {
+        //
         Gson gson = new Gson();
         BufferedReader br = new BufferedReader((new FileReader("/net/u/mah/openbaton/file.json")));
         Type mapOfMapsType = new TypeToken<Map<String, ServiceFunctionForwarders>>() {}.getType();
